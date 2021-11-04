7b6f155599a84ad85571ab602fa0318d67b16320
albert.fang <albert.fang@51job.com>
2021-11-04 10:48:03 +0800

    1、所有阶段的ppts 2、poi对ppt的使用

--- foodie-dev-api/src/test/java/RenameTest.java
+++ foodie-dev-api/src/test/java/RenameTest.java
@@ -1,6 +1,13 @@
+import org.apache.poi.hslf.usermodel.HSLFSlide;
+import org.apache.poi.hslf.usermodel.HSLFSlideShow;
+import org.apache.poi.sl.usermodel.Slide;
+import org.apache.poi.sl.usermodel.SlideShow;
+import org.apache.poi.sl.usermodel.SlideShowFactory;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * @ProjectName: foodie-dev
@@ -11,6 +18,58 @@ import java.io.File;
  * @Date: 2021/9/28 17:56
  */
 public class RenameTest {
+
+//    @Test
+    public void createPPTByXSLF() throws IOException {
+        String parentFilePath = "E:\\IdeaProjects\\foodie-dev\\doc\\ppt\\第二阶段\\";
+        new File(parentFilePath).mkdirs();
+        for (int i = 10; i < 25; i++) {
+            String finalFile = parentFilePath + (i < 10 ? "0" + i : i) + " .pptx";
+//        String finalFile = parentFilePath + "test.pptx";
+        // create a new empty slide show
+        XMLSlideShow ppt = new XMLSlideShow();
+        // add first slide
+        XSLFSlide blankSlide = ppt.createSlide();
+        // add second slide
+        XSLFSlide anotherSlide = ppt.createSlide();
+        FileOutputStream out = new FileOutputStream(finalFile);
+        ppt.write(out);
+        out.close();
+        }
+
+    }
+
+//    @Test
+    public void createPPTByHSLF() throws IOException {
+        String parentFilePath = "E:\\IdeaProjects\\foodie-dev\\doc\\ppt\\第六阶段\\";
+//        for (int i = 1; i < 2; i++) {
+//            String finalFile = parentFilePath + (i < 10 ? "0" + i : i) + " .pptx";
+            String finalFile = parentFilePath + "test.pptx";
+            //create a new empty slide show
+            HSLFSlideShow ppt = new HSLFSlideShow();
+            //add first slide
+            HSLFSlide s1 = ppt.createSlide();
+            //add second slide
+            HSLFSlide s2 = ppt.createSlide();
+            //save changes in a file
+            FileOutputStream out = new FileOutputStream(finalFile);
+            ppt.write(out);
+            out.close();
+//        }
+
+    }
+
+
+//    @Test
+    public void createPPT() throws IOException {
+        String parentFilePath = "E:\\IdeaProjects\\foodie-dev\\doc\\ppt\\第六阶段\\";
+        new File(parentFilePath).mkdirs();
+        for (int i = 1; i < 25; i++) {
+            String finalFile = parentFilePath + (i < 10 ? "0" + i : i) + " .pptx";
+            File file = new File(finalFile);
+            file.createNewFile();
+        }
+    }
 
 //    @Test
     public void pdfRenameTest(){
